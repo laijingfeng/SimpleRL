1d5a9f82640a1c1a71aacf122cd5af36e0036c35
laijingfeng <337292616@qq.com>
2017-07-18 21:01:15 +0800

    update

---
+++ Assets/Agent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Enviroment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment : MonoBehaviour
+{
+    private Transform panel;
+    private Transform prefab;
+
+    private Agent agent;
+    private List<Chest> chestList = new List<Chest>();
+
+    void Awake()
+    {
+        panel = this.transform.FindChild("Panel");
+        prefab = this.transform.FindChild("Prefab");
+        prefab.gameObject.SetActive(false);
+    }
+}
